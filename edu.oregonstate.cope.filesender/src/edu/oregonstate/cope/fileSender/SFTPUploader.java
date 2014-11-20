6fb176b143fd25942611b934f5143378b6544f3c
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-11-20 15:27:25 -0800

    Throttling the upload rate
    Now it's fixed at 200 KBs, but it will be configurable soon

--- edu.oregonstate.cope.filesender/src/edu/oregonstate/cope/fileSender/SFTPUploader.java
+++ edu.oregonstate.cope.filesender/src/edu/oregonstate/cope/fileSender/SFTPUploader.java
@@ -3,7 +3,10 @@ package edu.oregonstate.cope.fileSender;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.UnknownHostException;
+
+import org.java_bandwidthlimiter.StreamManager;
 
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
@@ -11,6 +14,8 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
+
+import edu.oregonstate.cope.clientRecorder.util.COPELogger;
 
 public class SFTPUploader {
 	
@@ -21,6 +26,10 @@ public class SFTPUploader {
 	private int port;
 	private String username = "";
 	private String password = "";
+	
+	private long uploadLimit = 200;
+	private boolean shouldLimit = true;
+	private StreamManager streamManager = null;
 	
 	private void initializeSession(String host, int port, String username, String password) throws UnknownHostException, JSchException {
 		this.host = host;
@@ -39,7 +48,9 @@ public class SFTPUploader {
 		Channel channel = this.session.openChannel("sftp");
 		channel.connect();
 		this.channelSftp = (ChannelSftp) channel;
-	
+		
+		streamManager = new StreamManager(uploadLimit * StreamManager.OneKbps * 8);
+		streamManager.enable();
 	}
 	
 	public SFTPUploader(String host, int port, String username, String password) throws UnknownHostException, JSchException {
@@ -72,7 +83,14 @@ public class SFTPUploader {
 
 		for(File file : files) {
 			if(file.isFile()) {
-				this.channelSftp.put(new FileInputStream(file), file.getName());
+				InputStream fileInputStream = new FileInputStream(file);
+				InputStream inputStream;
+				if (shouldLimit) {
+					inputStream = streamManager.registerStream(fileInputStream);
+				} else {
+					inputStream = fileInputStream;
+				}
+				this.channelSftp.put(inputStream, file.getName());
 			} else {	
 				this.createRemoteDir(file.getName());
 				this.uploadPathToFTP(localPath + File.separator + file.getName(),  file.getName());
